c1ba0a0ea2163c6f82a69635812683d6d6407662
Sarah Yao <ww.sarah7150@gmail.com>
2023-06-08 14:28:44 -0500

    I think its working

--- test.cc
+++ test.cc
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <bitset>
 
 // Struct to store the extracted data
 struct TensorData {
@@ -13,7 +14,7 @@ struct TensorData {
 int main() {
     // Open the binary file in read mode
     std::cout << "Hello world!" << std::endl;
-    std::ifstream file("../output16.bin", std::ios::binary);
+    std::ifstream file("../output.bin", std::ios::binary);
     
     if (!file) {
         std::cerr << "Failed to open the file." << std::endl;
@@ -27,9 +28,9 @@ int main() {
     while (true) {
         std::cout << "staart of while loop!" << std::endl;
         // Read the text data (name of weight tensor)
-        char text_data[11];
-        file.read(text_data, 10);
-        text_data[10] = '\0';
+        char text_data[51];
+        file.read(text_data, 50);
+        text_data[50] = '\0';
         std::string name(text_data);
         
         // If no more data to read, break the loop
@@ -40,26 +41,39 @@ int main() {
         // Read the binary data (size of tensor)
         uint32_t size;
         file.read(reinterpret_cast<char*>(&size), sizeof(uint32_t));
+        std::cout << "name: " << name << std::endl;
+        std::cout << "size: " << size << std::endl;
         
         // Read the tensor dataf
         std::vector<uint16_t> tensor(size);
-        std::cout << "size of uint16t:" << sizeof(uint16_t) << std::endl;
+        std::string layer31freqs = "layers.31.attention.inner_attention.rope.freqs    ";
         file.read(reinterpret_cast<char*>(tensor.data()), size * sizeof(uint16_t));
+        if (name.compare(layer31freqs) == 0) {
+          std::cout << "tensor: ";
+          for (const auto& element : tensor) {
+              std::bitset<16> binary(element);
+              std::cout << binary << " ";
+          }
+          std::cout << std::endl;
+        }
         
         // Store the extracted data
         extracted_data.push_back({name, size, tensor});
     }
-    
-    // Print the extracted data
-    for (const auto& data : extracted_data) {
-        std::cout << "Name of weight tensor: " << data.name << std::endl;
-        std::cout << "Size of tensor: " << data.size << std::endl;
-        std::cout << "Tensor: ";
-        for (float value : data.tensor) {
-            std::cout << value << " ";
-        }
-        std::cout << std::endl << std::endl;
-    }
+    // int i = 0;
+    // // Print the extracted data
+    // for (const auto& data : extracted_data) {
+    //     if (i == 2) {
+    //       break;
+    //     }
+    //     std::cout << "Name of weight tensor: " << data.name << std::endl;
+    //     std::cout << "Size of tensor: " << data.size << std::endl;
+    //     std::cout << "Tensor: ";
+    //     for (float value : data.tensor) {
+    //         std::cout << value << " ";
+    //     }
+    //     std::cout << std::endl << std::endl;
+    // }
     
     return 0;
 }
